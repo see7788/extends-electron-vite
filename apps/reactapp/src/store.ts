bb5ca5f6bc68d1170ed8ff605546f7b49144867f
see7788 <kitchens@qq.com>
2026-07-22 00:18:23 +0800

    feat: add tpl2 store slice

--- apps/reactapp/src/store.ts
+++ apps/reactapp/src/store.ts
@@ -4,15 +4,18 @@ import { immer } from "zustand/middleware/immer";
 import createFile from "./file/store";
 import createSse from "./sse/store";
 import createTpl from "./tpl/store";
+import createTpl2 from "./tpl2/store";
 import createTodotree from "./todotree/store";
 export default create<ReturnType<typeof createFile>
   & ReturnType<typeof createSse>
   & ReturnType<typeof createTpl>
+  & ReturnType<typeof createTpl2>
   & ReturnType<typeof createTodotree>
   >()(persist(immer((set, get, api) => ({
     ...createFile(set, get, api),
     ...createSse(set, get, api),
     ...createTpl(set, get, api),
+    ...createTpl2(set, get, api),
     ...createTodotree(set, get, api),
   })), {
     name: "extends-codex",
