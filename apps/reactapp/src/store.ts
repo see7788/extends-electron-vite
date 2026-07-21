5d8cca0ac75e4d137d83f44071d0f082bf00d6f6
see7788 <kitchens@qq.com>
2026-07-21 21:51:01 +0800

    refactor: simplify react app store

--- apps/reactapp/src/store.ts
+++ apps/reactapp/src/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import createFile from "./file/store";
 import createSse from "./sse/store";
@@ -9,20 +9,11 @@ export default create<ReturnType<typeof createFile>
   & ReturnType<typeof createSse>
   & ReturnType<typeof createTpl>
   & ReturnType<typeof createTodotree>
-  >()(persist(immer((set, get, api) => {
-    return {
-      ...createFile(set, get, api),
-      ...createSse(set, get, api),
-      ...createTpl(set, get, api),
-      ...createTodotree(set, get, api)
-    }
-  }), {
+  >()(persist(immer((set, get, api) => ({
+    ...createFile(set, get, api),
+    ...createSse(set, get, api),
+    ...createTpl(set, get, api),
+    ...createTodotree(set, get, api),
+  })), {
     name: "extends-codex",
-    storage: createJSONStorage(() => localStorage),
-    partialize: store => {
-      return Object.fromEntries(
-        Object.entries(store).filter(([storeKey]) => !storeKey.endsWith("Actions")),
-      )
-    },
-    onRehydrateStorage: () => (state) => { },
   }));
