8a1bb934f036ca26d559ec41f8b04dd459a62a84
see7788 <kitchens@qq.com>
2026-07-25 01:07:56 +0800

    checkpoint: save MCP consumer integration

--- apps/honoapp/src/store.ts
+++ apps/honoapp/src/store.ts
@@ -4,10 +4,12 @@ import tplStore, { type TplStore } from "./tpl/store";
 import globalTplStore, { type GlobalTplStore } from "./tpl/global/store";
 import tpl2Store, { type Tpl2Store } from "./tpl2/store";
 import { createStore } from "zustand";
+import McpServer from "extends-hono/createMcpServer/index"
 import { immer } from "zustand/middleware/immer";
 import { homedir } from "node:os";
 import path from "node:path"
 import pkg from "../package.json"
+export const mcpServer=new McpServer({version:pkg.version,name:pkg.name})
 export type Store = ChatStore & TplStore & GlobalTplStore & Tpl2Store & {
   runtimeActions: {
     hostname: string;
@@ -17,15 +19,15 @@ export type Store = ChatStore & TplStore & GlobalTplStore & Tpl2Store & {
 
 export default createStore<Store>()(
   cwdPersist({
-    cwd:path.join(homedir(),"extends-electron-vite"),
-    name:pkg.name,
+    cwd:homedir(),
+    name:["extends-electron-vite",pkg.name].join("."),
     initializer: immer<Store>((set, get, api) => ({
       runtimeActions: {
         hostname: "127.0.0.1",
         port: 3005
       },
       ...chatStore(set, get),
-      ...tplStore(set, get),
+      ...tplStore(set, get, api),
       ...globalTplStore(set, get),
       ...tpl2Store(set, get, api),
     })),
