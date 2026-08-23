30783e08640eeb08d2147a95486db582f8adf438
see7788 <kitchens@qq.com>
2026-08-23 13:11:21 +0800

    2026-08-23 13:11:20 自动提交

--- libs/electron-mcpserver/slices.ts
+++ libs/electron-mcpserver/slices.ts
@@ -2,7 +2,7 @@ import mcpserver from "mcpserver";
 import electronViteConfig from "./electron-vite-config/index";
 import runtimeproxy from "./runtimeproxy/index";
 
-export default mcpserver.packageImport({
+export default mcpserver.import({
   packageName: "electron-mcpserver",
   description: "为 Electron Vite 项目提供配置与 runtime proxy 工具。",
   RegisterAny: [electronViteConfig, runtimeproxy],
