41fb32557ebcc5a1714675c3bb43702625d35b23
see7788 <kitchens@qq.com>
2026-09-09 10:34:18 +0800

    refactor: rename metas collection method

--- libs/electron-vite-config-lib/slices.ts
+++ libs/electron-vite-config-lib/slices.ts
@@ -3,5 +3,5 @@ import electronViteConfig from "./electron-vite-config/index";
 import runtimeProxy from "./runtimeproxy/index";
 
 export default mcpserver.metas("electron")
-  .register(electronViteConfig, runtimeProxy)
+  .metas(electronViteConfig, runtimeProxy)
   .import(["electron", "Electron application, IPC, and Vite configuration capabilities."]);
